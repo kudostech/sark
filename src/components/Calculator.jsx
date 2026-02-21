3dc03c61f010945de57fec4998c5760d1d0b1d3a
Kudos Tech <abolajiayobami2000@gmail.com>
2026-02-21 12:04:36 +0000

    feat: create a cost calculator section for mobile

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,82 +1,349 @@
 import React from "react";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import pricetag from "../assets/pricetag.png";
 function Calculator() {
-
   const [kmValue, setKmValue] = useState(500);
-    const [hrValue, setHrValue] = useState(30);
-return (
-    <div className="md:hidden">
-      <section className="  mx-8 p-4 rounded-2xl mt-10 border border-[#eeeeee]">
-        <div className=" gap-8 ">
-          <h1 className="text-green-600 font-light">COST CALCULATOR</h1>
+  const [hrValue, setHrValue] = useState(30);
+  return (
+    <div >
+      <div className=" md:hidden">
+        <section className="  mx-10  rounded-2xl mt-10 border border-[#eeeeee]">
+          <div className=" gap-8 p-4 ">
+            <h1 className="text-green-600 font-light">COST CALCULATOR</h1>
 
-         <div>
-             <div>
-            <div className=" flex justify-between mt-3">
-            <p className=" text-[#8e8e8e] font-extralight">
-              {" "}
-              Hoeveel kilometers <br />
-              reis je per maand?
-            </p>
-            <button className=" rounded-3xl py-1 px-10 bg-[#f4f4f4]">
-            {kmValue}
-            </button>
-          </div>
-          <input 
-       value={kmValue}
-          max= "1500"
-          min="0"
-          onChange={(e) => setKmValue(e.target.value)}
-          className="
+            <div>
+              <div>
+                <div className=" flex justify-between mt-3">
+                  <p className=" text-[#8e8e8e] font-extralight">
+                    {" "}
+                    Hoeveel kilometers <br />
+                    reis je per maand?
+                  </p>
+                  <button className=" rounded-3xl py-1 px-10 bg-[#f4f4f4]">
+                    {kmValue}
+                  </button>
+                </div>
+                <input
+                  value={kmValue}
+                  max="1500"
+                  min="0"
+                  onChange={(e) => setKmValue(e.target.value)}
+                  className="
           rounded-md
           appearance-none
           bg-gray-200 h-0.5
           cursor-pointer
            w-full mt-6 accent-black 
            "
-            style={{
-          background: `linear-gradient(to right, black ${
-            (kmValue / 1500) * 100
-          }%, #e5e7eb ${(kmValue / 1500) * 100}%)`,
-        }}
-          type="range" />
-          </div>
-         </div>
+                  style={{
+                    background: `linear-gradient(to right, black ${
+                      (kmValue / 1500) * 100
+                    }%, #e5e7eb ${(kmValue / 1500) * 100}%)`,
+                  }}
+                  type="range"
+                />
+              </div>
+            </div>
 
-         <div>
-             <div>
-            <div className=" flex justify-between mt-3">
-            <p className=" text-[#8e8e8e] font-extralight">
-              {" "}
-              Hoeveel uur gebruik je <br /> de auto per maand?
-            </p>
-            <button className=" rounded-3xl py-1 px-12 bg-[#f4f4f4]">
-            {hrValue}
-            </button>
-          </div>
+            <div>
+              <div>
+                <div className=" flex justify-between mt-3">
+                  <p className=" text-[#8e8e8e] font-extralight">
+                    {" "}
+                    Hoeveel uur gebruik je <br /> de auto per maand?
+                  </p>
+                  <button className=" rounded-3xl py-1 px-12 bg-[#f4f4f4]">
+                    {hrValue}
+                  </button>
+                </div>
 
-          <input 
-         value={hrValue}
-          max= "300"
-          min="0"
-          onChange={(e) => setHrValue(e.target.value)}
-          className="
+                <input
+                  value={hrValue}
+                  max="300"
+                  min="0"
+                  onChange={(e) => setHrValue(e.target.value)}
+                  className="
           rounded-md
           appearance-none
           bg-gray-200 h-0.5
           cursor-pointer
            w-full mt-6 accent-black 
            "
-            style={{
-          background: `linear-gradient(to right, black ${
-            (hrValue / 300) * 100
-          }%, #e5e7eb ${(hrValue / 300) * 100}%)`,
-        }}
-          type="range" />
+                  style={{
+                    background: `linear-gradient(to right, black ${
+                      (hrValue / 300) * 100
+                    }%, #e5e7eb ${(hrValue / 300) * 100}%)`,
+                  }}
+                  type="range"
+                />
+              </div>
+            </div>
           </div>
-         </div>
+          <hr className=" w-full mt-3 text-[#e9e9e9] " />
+
+          <div className="p-4  ">
+            <div className=" flex-1  ">
+              <h6 className="text-green-500 font-medium ">CAGO COSTS:</h6>
+              <h1 className="text-4xl font-bold mt-4 text-[#494949] "> €220</h1>
+              <p className="text-xs mt-2">
+                {" "}
+                It is recommended to use Cago <br /> PLUS subscription to save
+                more.{" "}
+              </p>
+            </div>
+            <hr className="  my-4 text-[#e9e9e9] " />
+            <div className=" flex-1  ">
+              <h6 className=" text-sm ">PERSONAL CAR COSTS:</h6>
+              <div className=" flex items-center mt-4 gap-1">
+                <h1 className="text-4xl font-bold  text-[#494949] "> €400</h1>
+                <FontAwesomeIcon
+                  className="mt-2 text-xl"
+                  icon={faCircleExclamation}
+                />
+              </div>
+              <p className="text-xs mt-2">
+                {" "}
+                Berekend op basis van bezit en <br /> gebruik van een VW Polo
+                uit uit 2020
+              </p>
+            </div>
+          </div>
+          <div className=" bg-green-50  mb-6 mx-4 rounded-md py-3 px-4">
+            <p className=" text-sm ">
+              Driving Cago{" "}
+              <span
+                className=" underline decoration-gray-500
+"
+              >
+                luxurious car,
+              </span>{" "}
+              <br /> you will{" "}
+              <span className=" text-green-600"> save per month:</span>
+            </p>
+            <div className=" mt-2 flex gap-2  items-center ">
+              <h1 className=" text-4xl font-bold text-green-600"> €180</h1>{" "}
+              <img className=" h-10" src={pricetag} alt="pricetag" />
+            </div>
+          </div>
+        </section>
+        <div className=" bg-gray-50  mt-4 mx-10 rounded-md">
+          <h1 className=" font-bold p-4">
+            {" "}
+            We take care of everything, you don’t <br />
+            have to worry about:
+          </h1>
+
+          <section className=" gap-3 flex flex-col">
+            <div className=" gap-1 ml-4 mr-34 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className=" font-extralight text-sm"> Charging or refuelling</p>
         </div>
-      </section>
+        <div className=" gap-1 ml-4 mr-56 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Insurance</p>
+        </div>
+        <div className=" gap-1 ml-4 mr-48 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> APK Inspection</p>
+        </div>
+        <div className=" flex mx-4 gap-3">
+            <div className=" gap-1 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Maintenance</p>
+        </div>
+        <div className=" gap-1  border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Road tax</p>
+        </div>
+        </div>
+         <div className=" gap-1 ml-4 mr-36 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Increasing parking fee</p>
+        </div>
+        <div className=" gap-1 ml-4 mr-40 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Roadside assistance</p>
+        </div>
+
+        <div className=" gap-1 ml-4 mr-46 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Car depreciation</p>
+        </div>
+          </section>
+        </div>
+        
+      </div>
+
+
+       <div className=" hidden md:block ">
+        <section className="  mx-10  rounded-2xl mt-10 border border-[#eeeeee]">
+          <div className=" gap-8 p-4 ">
+            <h1 className="text-green-600 font-light">COST CALCULATOR</h1>
+
+            <div>
+              <div>
+                <div className=" flex justify-between mt-3">
+                  <p className=" text-[#8e8e8e] font-extralight">
+                    {" "}
+                    Hoeveel kilometers <br />
+                    reis je per maand?
+                  </p>
+                  <button className=" rounded-3xl py-1 px-10 bg-[#f4f4f4]">
+                    {kmValue}
+                  </button>
+                </div>
+                <input
+                  value={kmValue}
+                  max="1500"
+                  min="0"
+                  onChange={(e) => setKmValue(e.target.value)}
+                  className="
+          rounded-md
+          appearance-none
+          bg-gray-200 h-0.5
+          cursor-pointer
+           w-full mt-6 accent-black 
+           "
+                  style={{
+                    background: `linear-gradient(to right, black ${
+                      (kmValue / 1500) * 100
+                    }%, #e5e7eb ${(kmValue / 1500) * 100}%)`,
+                  }}
+                  type="range"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <div className=" flex justify-between mt-3">
+                  <p className=" text-[#8e8e8e] font-extralight">
+                    {" "}
+                    Hoeveel uur gebruik je <br /> de auto per maand?
+                  </p>
+                  <button className=" rounded-3xl py-1 px-12 bg-[#f4f4f4]">
+                    {hrValue}
+                  </button>
+                </div>
+
+                <input
+                  value={hrValue}
+                  max="300"
+                  min="0"
+                  onChange={(e) => setHrValue(e.target.value)}
+                  className="
+          rounded-md
+          appearance-none
+          bg-gray-200 h-0.5
+          cursor-pointer
+           w-full mt-6 accent-black 
+           "
+                  style={{
+                    background: `linear-gradient(to right, black ${
+                      (hrValue / 300) * 100
+                    }%, #e5e7eb ${(hrValue / 300) * 100}%)`,
+                  }}
+                  type="range"
+                />
+              </div>
+            </div>
+          </div>
+          <hr className=" w-full mt-3 text-[#e9e9e9] " />
+
+          <div className="p-4  ">
+            <div className=" flex-1  ">
+              <h6 className="text-green-500 font-medium ">CAGO COSTS:</h6>
+              <h1 className="text-4xl font-bold mt-4 text-[#494949] "> €220</h1>
+              <p className="text-xs mt-2">
+                {" "}
+                It is recommended to use Cago <br /> PLUS subscription to save
+                more.{" "}
+              </p>
+            </div>
+            <hr className="  my-4 text-[#e9e9e9] " />
+            <div className=" flex-1  ">
+              <h6 className=" text-sm ">PERSONAL CAR COSTS:</h6>
+              <div className=" flex items-center mt-4 gap-1">
+                <h1 className="text-4xl font-bold  text-[#494949] "> €400</h1>
+                <FontAwesomeIcon
+                  className="mt-2 text-xl"
+                  icon={faCircleExclamation}
+                />
+              </div>
+              <p className="text-xs mt-2">
+                {" "}
+                Berekend op basis van bezit en <br /> gebruik van een VW Polo
+                uit uit 2020
+              </p>
+            </div>
+          </div>
+          <div className=" bg-green-50  mb-6 mx-4 rounded-md py-3 px-4">
+            <p className=" text-sm ">
+              Driving Cago{" "}
+              <span
+                className=" underline decoration-gray-500
+"
+              >
+                luxurious car,
+              </span>{" "}
+              <br /> you will{" "}
+              <span className=" text-green-600"> save per month:</span>
+            </p>
+            <div className=" mt-2 flex gap-2  items-center ">
+              <h1 className=" text-4xl font-bold text-green-600"> €180</h1>{" "}
+              <img className=" h-10" src={pricetag} alt="pricetag" />
+            </div>
+          </div>
+        </section>
+        <div className=" bg-gray-50  mt-4 mx-10 rounded-md">
+          <h1 className=" font-bold p-4">
+            {" "}
+            We take care of everything, you don’t <br />
+            have to worry about:
+          </h1>
+
+          <section className=" gap-3 flex flex-col">
+            <div className=" gap-1 ml-4 mr-34 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className=" font-extralight text-sm"> Charging or refuelling</p>
+        </div>
+        <div className=" gap-1 ml-4 mr-56 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Insurance</p>
+        </div>
+        <div className=" gap-1 ml-4 mr-48 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> APK Inspection</p>
+        </div>
+        <div className=" flex mx-4 gap-3">
+            <div className=" gap-1 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Maintenance</p>
+        </div>
+        <div className=" gap-1  border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Road tax</p>
+        </div>
+        </div>
+         <div className=" gap-1 ml-4 mr-36 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Increasing parking fee</p>
+        </div>
+        <div className=" gap-1 ml-4 mr-40 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Roadside assistance</p>
+        </div>
+
+        <div className=" gap-1 ml-4 mr-46 border border-[#e9e9e9] p-1 flex items-center bg-gray-100  pl-2  rounded-2xl">
+            <FontAwesomeIcon className=" text-green-600" icon={faCircleCheck}/>
+            <p className="font-extralight text-sm"> Car depreciation</p>
+        </div>
+          </section>
+        </div>
+        
+      </div>
     </div>
   );
 }
